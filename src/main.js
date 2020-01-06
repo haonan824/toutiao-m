6006a4f02efccb3cd87ef2815329a4fce4979517
hhn <1761293315@qq.com>
2020-01-06 15:07:39 +0800

    项目初始化-调整结构

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import vuex from 'vuex'
 Vue.config.productionTip = false
-
+Vue.use(vuex)
 new Vue({
   router,
   store,
